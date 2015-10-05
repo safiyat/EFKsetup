46a7ddaee275019331bcf1932c6dbd44324c72b8
Safiyat Reza <md.reza@snapdeal.com>
2015-10-05 11:36:54 +0530

    Refactored the code. Added curl to list.

--- pre-setup.sh
+++ pre-setup.sh
@@ -1,3 +1,45 @@
-sudo apt-get -y install ruby2.0
-sudo apt-get -y install bundler
-sudo apt-get -y install ntp
+APT_UPDATED="True"
+
+CURL_INSTALLED=$(dpkg -l | grep " curl ")
+if [ -z  "$CURL_INSTALLED" ]
+then
+    if [ -z $APT_UPDATED ]
+    then
+        sudo apt-get update
+        APT_UPDATED="True"
+    fi
+    sudo apt-get -y install curl
+fi
+
+#RUBY_INSTALLED=$(dpkg -l | grep " ruby2.0 ")
+#if [ -z  "$RUBY_INSTALLED" ]
+#then
+#    if [ -z $APT_UPDATED ]
+#    then
+#        sudo apt-get update
+#        APT_UPDATED="True"
+#    fi
+#    sudo apt-get -y install ruby2.0
+#fi
+
+#BUNDLER_INSTALLED=$(dpkg -l | grep " bundler ")
+#if [ -z  "$BUNDLER_INSTALLED" ]
+#then
+#    if [ -z $BUNDLER_UPDATED ]
+#    then
+#        sudo apt-get update
+#        APT_UPDATED="True"
+#    fi
+#    sudo apt-get -y install bundler
+#fi
+
+#NTP_INSTALLED=$(dpkg -l | grep " ntp ")
+#if [ -z  "$NTP_INSTALLED" ]
+#then
+#    if [ -z $APT_UPDATED ]
+#    then
+#        sudo apt-get update
+#        APT_UPDATED="True"
+#    fi
+#    sudo apt-get -y install ntp
+#fi
