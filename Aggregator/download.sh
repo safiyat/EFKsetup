288a06c2bf48f74b16e56923ce11035837083da8
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-29 13:27:54 +0530

    Restructuring.

---
+++ Aggregator/download.sh
@@ -0,0 +1,24 @@
+#Download elasticsearch
+if [ ! -s  "elasticsearch-1.7.2.tar.gz" ]
+then
+    curl -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz
+fi
+#Download elasticsearch-head
+if [ ! -s  "elasticsearch-head-master.zip" ]
+then
+    curl -O https://codeload.github.com/mobz/elasticsearch-head/zip/master
+    mv master elasticsearch-head-master.zip
+fi
+
+# #Download fluentd
+# if [ ! -s  "fluentd-master.zip" ]
+# then
+#     curl -O https://codeload.github.com/fluent/fluentd/zip/master
+#     mv master fluentd-master.zip
+# fi
+
+#Download kibana
+if [ ! -s  "kibana-4.1.2-linux-x64.tar.gz" ]
+then
+    curl -O https://download.elastic.co/kibana/kibana/kibana-4.1.2-linux-x64.tar.gz
+fi
---
+++ Collector/fluentd_setup.sh
@@ -0,0 +1,14 @@
+# Increase the maximum number of file descriptors
+echo -e "root soft nofile 65536\nroot hard nofile 65536\n* soft nofile 65536\n* hard nofile 65536" | sudo tee -a /etc/security/limits.conf > /dev/null
+
+curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-trusty-td-agent2.sh | sh
+
+
+sudo /etc/init.d/td-agent restart
+sudo /etc/init.d/td-agent status
+
+#sudo apt-get -y install ruby2.0 ruby2.0-dev
+
+#sdo gem install fluentd --no-ri --no-rdoc
+
+#fluentd --setup ./fluent
