1c0ecaaeeeabf53de50f0198bf6fcf4186eb3fa0
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-29 13:31:40 +0530

    Restructuring complete.

--- Collector/pre-setup.sh
+++ Collector/pre-setup.sh
@@ -11,33 +11,33 @@ then
     sudo apt-get -y install curl
 fi
 
-RUBY_INSTALLED=$(dpkg -l | grep " ruby2.0 ")
-if [ -z  "$RUBY_INSTALLED" ]
-then
-    if [ -z $APT_UPDATED ]
-    then
-        sudo apt-get update
-        APT_UPDATED="True"
-    fi
-    sudo apt-get -y install ruby2.0 ruby2.0-dev
-fi
+# RUBY_INSTALLED=$(dpkg -l | grep " ruby2.0 ")
+# if [ -z  "$RUBY_INSTALLED" ]
+# then
+#     if [ -z $APT_UPDATED ]
+#     then
+#         sudo apt-get update
+#         APT_UPDATED="True"
+#     fi
+#     sudo apt-get -y install ruby2.0 ruby2.0-dev
+# fi
 
-BUNDLER_INSTALLED=$(dpkg -l | grep " bundler ")
-if [ -z  "$BUNDLER_INSTALLED" ]
-then
-    if [ -z $APT_UPDATED ]
-    then
-        sudo apt-get update
-        APT_UPDATED="True"
-    fi
-    sudo apt-get -y install bundler
-fi
+# BUNDLER_INSTALLED=$(dpkg -l | grep " bundler ")
+# if [ -z  "$BUNDLER_INSTALLED" ]
+# then
+#     if [ -z $APT_UPDATED ]
+#     then
+#         sudo apt-get update
+#         APT_UPDATED="True"
+#     fi
+#     sudo apt-get -y install bundler
+# fi
 
-RDOC_INSTALLED=$(gem2.0 list --local | grep "^rdoc ")
-if [ -z  "$BUNDLER_INSTALLED" ]
-then
-    sudo gem2.0 install rdoc
-fi
+# RDOC_INSTALLED=$(gem2.0 list --local | grep "^rdoc ")
+# if [ -z  "$BUNDLER_INSTALLED" ]
+# then
+#     sudo gem2.0 install rdoc
+# fi
 
 NTP_INSTALLED=$(dpkg -l | grep " ntp ")
 if [ -z  "$NTP_INSTALLED" ]
@@ -50,13 +50,13 @@ then
     sudo apt-get -y install ntp
 fi
 
-JAVA_INSTALLED=$(which java)
-if [ -z  "$JAVA_INSTALLED" ]
-then
-    if [ -z "$APT_UPDATED" ]
-    then
-        sudo apt-get update
-        APT_UPDATED="True"
-    fi
-    sudo apt-get -y install openjdk-7-jre
-fi
+# JAVA_INSTALLED=$(which java)
+# if [ -z  "$JAVA_INSTALLED" ]
+# then
+#     if [ -z "$APT_UPDATED" ]
+#     then
+#         sudo apt-get update
+#         APT_UPDATED="True"
+#     fi
+#     sudo apt-get -y install openjdk-7-jre
+# fi
