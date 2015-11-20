1f974951a2c55b5c7ab4164fbaf5d9d70243dcb5
Safiyat Reza <md.reza@snapdeal.com>
2015-11-20 14:06:17 +0530

    Finished.

--- Collector/workaround/install_gems.sh
+++ Collector/workaround/install_gems.sh
@@ -9,20 +9,23 @@ if [ -n "$1" ]
 then
     GEM_PATH="$1"
 else
-    GEM_PATH="~/gems/"
+    GEM_PATH="~/gems"
 fi
 
-if [ -f "$GEM_PATH/fluent-plugin-retag*.gem" ]
+echo "GEMPATH:-$GEM_PATH-"
+
+if [ -f "$GEM_PATH/fluent-plugin-retag-0.0.1.gem" ]
 then
-    sudo td-agent-gem install fluent-plugin-retag --bindir $GEM_PATH
+    echo "sudo td-agent-gem install fluent-plugin-retag --local --bindir $GEM_PATH"
+    sudo td-agent-gem install fluent-plugin-retag --local --bindir $GEM_PATH
 else
     echo "Gem fluent-plugin-retag not found. Not installing."
 fi
 
 
-if [ -f "$GEM_PATH/fluent-plugin-grok-parser*.gem" ]
+if [ -f "$GEM_PATH/fluent-plugin-grok-parser-0.0.2.gem" ]
 then
-    sudo td-agent-gem install fluent-plugin-grok-parser --bindir $GEM_PATH
+    sudo td-agent-gem install fluent-plugin-grok-parser --local --bindir $GEM_PATH
 else
     echo "Gem fluent-plugin-grok-parser not found. Not installing."
 fi
