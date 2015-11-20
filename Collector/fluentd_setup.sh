cc66cbd3cc96195f39e8d78002a0e0ced954b33d
Safiyat Reza <md.reza@snapdeal.com>
2015-11-20 11:44:18 +0530

    Update fluentd_setup.sh

--- Collector/fluentd_setup.sh
+++ Collector/fluentd_setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 # Download and install fluentd (td-agent) and all its requirements
-curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-trusty-td-agent2.sh | sh
+# curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-trusty-td-agent2.sh | sh
+
+curl https://packages.treasuredata.com/GPG-KEY-td-agent | apt-key add -
+echo "deb [arch=amd64] http://packages.treasuredata.com/2/ubuntu/trusty/ trusty contrib" > /etc/apt/sources.list.d/treasure-data.list
+apt-get update
+apt-get install -y --force-yes td-agent
 
 # Status check
 sudo /etc/init.d/td-agent restart
