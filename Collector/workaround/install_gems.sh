28e87506497585894f01c11f4425230677b0d6b2
Ubuntu User <ubuntu@Ubuntu-5.local.lan>
2015-11-24 18:16:06 +0530

    Overridden the gem install code.

--- Collector/workaround/install_gems.sh
+++ Collector/workaround/install_gems.sh
@@ -5,26 +5,28 @@
 #
 #
 
-if [ -n "$1" ]
-then
-    GEM_PATH="$1"
-else
-    GEM_PATH=`pwd`
-fi
+# if [ -n "$1" ]
+# then
+#     GEM_PATH="$1"
+# else
+#     GEM_PATH=`pwd`
+# fi
 
-echo "GEMPATH:-$GEM_PATH-"
+# echo "GEMPATH:-$GEM_PATH-"
 
-if [ -s "${GEM_PATH%%/}/fluent-plugin-retag-0.0.1.gem" ]
-then
-    sudo td-agent-gem install fluent-plugin-retag --local --bindir $GEM_PATH
-else
-    echo "Gem fluent-plugin-retag not found. Not installing."
-fi
+# if [ -s "${GEM_PATH%%/}/fluent-plugin-retag-0.0.1.gem" ]
+# then
+#     sudo td-agent-gem install fluent-plugin-retag --local --bindir $GEM_PATH
+# else
+#     echo "Gem fluent-plugin-retag not found. Not installing."
+# fi
 
 
-if [ -f "${GEM_PATH%%/}/fluent-plugin-grok-parser-0.0.2.gem" ]
-then
-    sudo td-agent-gem install fluent-plugin-grok-parser --local --bindir $GEM_PATH
-else
-    echo "Gem fluent-plugin-grok-parser not found. Not installing."
-fi
+# if [ -f "${GEM_PATH%%/}/fluent-plugin-grok-parser-0.0.2.gem" ]
+# then
+#     sudo td-agent-gem install fluent-plugin-grok-parser --local --bindir $GEM_PATH
+# else
+#     echo "Gem fluent-plugin-grok-parser not found. Not installing."
+# fi
+
+sudo td-agent-gem install --force --local /home/ubuntu/.gems/*.gem
