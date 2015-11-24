2b21a8c1177f59a84fb155f3016119d7bed505d8
Safiyat Reza <md.reza@snapdeal.com>
2015-11-24 15:16:24 +0530

    Diabled pre-configured stuff.

--- Collector/pre-setup.sh
+++ Collector/pre-setup.sh
@@ -13,24 +13,26 @@ then
     sudo apt-get -y install curl
 fi
 
-NTP_INSTALLED=$(dpkg -l | grep " ntp ")
-if [ -z  "$NTP_INSTALLED" ]
-then
-    if [ -z $APT_UPDATED ]
-    then
-        sudo apt-get update
-        APT_UPDATED="True"
-    fi
-    sudo apt-get -y install ntp
-fi
 
+# Pre-configured
+# NTP_INSTALLED=$(dpkg -l | grep " ntp ")
+# if [ -z  "$NTP_INSTALLED" ]
+# then
+#     if [ -z $APT_UPDATED ]
+#     then
+#         sudo apt-get update
+#         APT_UPDATED="True"
+#     fi
+#     sudo apt-get -y install ntp
+# fi
+
+
+# Pre-configured
 # Increase the maximum number of file descriptors
-
-
-if [[ "`ulimit -n`" != 65536 ]]
-then
-echo -e "root soft nofile 65536 \n\
-root hard nofile 65536\n\
-* soft nofile 65536 \n\
-* hard nofile 65536" | sudo tee -a /etc/security/limits.conf > /dev/null
-fi
+# if [[ "`ulimit -n`" != 65536 ]]
+# then
+# echo -e "root soft nofile 65536 \n\
+# root hard nofile 65536\n\
+# * soft nofile 65536 \n\
+# * hard nofile 65536" | sudo tee -a /etc/security/limits.conf > /dev/null
+# fi
